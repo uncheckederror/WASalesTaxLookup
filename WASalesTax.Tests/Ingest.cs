dd6648382a3f91c91efcdb9d8f68c1f3b0242dbe
Thomas Ryan <uncheckederror@users.noreply.github.com>
2021-03-31 19:56:25 -0700

    Disabled an integration test.

--- WASalesTax.Tests/Ingest.cs
+++ WASalesTax.Tests/Ingest.cs
@@ -34,41 +34,41 @@ namespace WASaleTax.Tests
             configuration = config;
         }
 
-        [Fact]
-        public async Task IngestDataAsync()
-        {
-            // Figure out the current period and the filename for the source data.
-            var period = new Period(DateTime.Now);
-            var stateFile = $"State_{period.Year.ToString().Substring(2)}Q{period.PeriodNumber}";
-            var zipBaseFile = $"ZIP4Q{period.PeriodNumber}{period.Year.ToString().Substring(2)}C";
-            var rateBaseFile = $"Rates_{period.Year.ToString().Substring(2)}Q{period.PeriodNumber}";
+        //[Fact]
+        //public async Task IngestDataAsync()
+        //{
+        //    // Figure out the current period and the filename for the source data.
+        //    var period = new Period(DateTime.Now);
+        //    var stateFile = $"State_{period.Year.ToString().Substring(2)}Q{period.PeriodNumber}";
+        //    var zipBaseFile = $"ZIP4Q{period.PeriodNumber}{period.Year.ToString().Substring(2)}C";
+        //    var rateBaseFile = $"Rates_{period.Year.ToString().Substring(2)}Q{period.PeriodNumber}";
 
-            // Delete the existing database if it exists and then recreate it.
-            await db.Database.EnsureDeletedAsync();
-            await db.Database.EnsureCreatedAsync();
+        //    // Delete the existing database if it exists and then recreate it.
+        //    await db.Database.EnsureDeletedAsync();
+        //    await db.Database.EnsureCreatedAsync();
 
-            // Ingest the data into the SQLite database.
-            var baseUrl = configuration.GetConnectionString("BaseDataUrl");
+        //    // Ingest the data into the SQLite database.
+        //    var baseUrl = configuration.GetConnectionString("BaseDataUrl");
 
-            var checkRates = await DataSource.TryIngestTaxRatesAsync($"{baseUrl}{rateBaseFile}.zip", db).ConfigureAwait(false);
-            var checkZip = await DataSource.TryIngestShortZipCodesAsync($"{baseUrl}{zipBaseFile}.zip", db).ConfigureAwait(false);
-            var checkAddresses = await DataSource.TryIngestAddressesAsync($"{baseUrl}{stateFile}.zip", db).ConfigureAwait(false);
+        //    var checkRates = await DataSource.TryIngestTaxRatesAsync($"{baseUrl}{rateBaseFile}.zip", db).ConfigureAwait(false);
+        //    var checkZip = await DataSource.TryIngestShortZipCodesAsync($"{baseUrl}{zipBaseFile}.zip", db).ConfigureAwait(false);
+        //    var checkAddresses = await DataSource.TryIngestAddressesAsync($"{baseUrl}{stateFile}.zip", db).ConfigureAwait(false);
 
-            Assert.True(checkRates);
-            Assert.True(checkZip);
-            Assert.True(checkAddresses);
+        //    Assert.True(checkRates);
+        //    Assert.True(checkZip);
+        //    Assert.True(checkAddresses);
 
-            var taxRateCount = await db.TaxRates.CountAsync();
-            var zipCount = await db.ZipCodes.CountAsync();
-            var addressCount = await db.AddressRanges.CountAsync();
+        //    var taxRateCount = await db.TaxRates.CountAsync();
+        //    var zipCount = await db.ZipCodes.CountAsync();
+        //    var addressCount = await db.AddressRanges.CountAsync();
 
-            Assert.True(taxRateCount > 0);
-            Assert.True(zipCount > 0);
-            Assert.True(addressCount > 0);
+        //    Assert.True(taxRateCount > 0);
+        //    Assert.True(zipCount > 0);
+        //    Assert.True(addressCount > 0);
 
-            output.WriteLine($"TaxRates: {taxRateCount}");
-            output.WriteLine($"ZipCodes: {zipCount}");
-            output.WriteLine($"AddressRanges: {addressCount}");
-        }
+        //    output.WriteLine($"TaxRates: {taxRateCount}");
+        //    output.WriteLine($"ZipCodes: {zipCount}");
+        //    output.WriteLine($"AddressRanges: {addressCount}");
+        //}
     }
 }
